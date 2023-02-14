76562591ab36da83ab8951261be627d0f6010437
Oleksandr Shliakhovyi <Oleksandr_Shliakhovyi@epam.com>
2023-02-14 13:29:11 +0200

    Switch db to postgresql

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "2.7.5"
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.1"
 
@@ -10,7 +12,7 @@ gem "rails", "~> 7.0.4", ">= 7.0.4.1"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+gem "pg", "~> 1.1"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
@@ -25,6 +27,5 @@ end
 
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+  gem "spring"
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_30_111552) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "branches", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "organization_id"
+    t.bigint "organization_id"
     t.index ["organization_id"], name: "index_branches_on_organization_id"
   end
 
@@ -27,7 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_111552) do
     t.datetime "experienced_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "branch_id"
+    t.bigint "branch_id"
     t.index ["branch_id"], name: "index_feedbacks_on_branch_id"
   end
 
